89bc6eef86b6fb0b70fef4bc472b7177d51ec005
msojocs <jiyecafe@gmail.com>
2024-07-06 22:44:38 +0800

    docker log

--- test/runner/nwjs/module-prepare.sh
+++ test/runner/nwjs/module-prepare.sh
@@ -55,6 +55,7 @@ until $(curl --output /dev/null --silent --head --fail http://127.0.0.1:8083/che
     if [ $i -ge 50 ];then
         echo "error"
         docker ps -a
+        docker logs wine
         exit 1
     fi
 done
