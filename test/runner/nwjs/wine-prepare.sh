ae97399c75f01bafee7e953087a9a741ccc6dfb2
msojocs <jiyecafe@gmail.com>
2024-07-06 10:25:01 +0800

    perf: 调整结构

---
+++ test/runner/nwjs/wine-prepare.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+set -ex
+root_dir=$(cd `dirname $0`/../../.. && pwd -P)
+cur_dir=$(cd `dirname $0` && pwd -P)
+
+nw_version="0.55.0"
+
+if [ ! -f "$root_dir/cache/nwjs-sdk-v$nw_version-win-x64.zip" ];then
+    wget -c -O "$root_dir/cache/nwjs-sdk-v$nw_version-win-x64.zip.tmp" "https://dl.nwjs.io/v0.55.0/nwjs-v0.55.0-win-x64.zip"
+    mv "$root_dir/cache/nwjs-sdk-v$nw_version-win-x64.zip.tmp" "$root_dir/cache/nwjs-sdk-v$nw_version-win-x64.zip"
+fi
+
+if [ ! -d "$root_dir/cache/nwjs-sdk-v$nw_version-win-x64" ];then
+    cd "$root_dir/cache"
+    unzip "nwjs-sdk-v$nw_version-win-x64.zip"
+fi
+
+rm -rf "$root_dir/cache/nwjs-sdk-v$nw_version-win-x64/package.nw"
+cp -r "$cur_dir/package.nw" "$root_dir/cache/nwjs-sdk-v$nw_version-win-x64"
+
+docker run -d -it\
+    --volume=$root_dir:/workspace\
+    --env=USE_XVFB=yes\
+    --env=XVFB_SERVER=:95\
+    --env=XVFB_SCREEN=0\
+    --env=XVFB_RESOLUTION=320x240x8\
+    --env=DISPLAY=:95\
+    --rm\
+    --hostname=DESKTOP-1TV4OAG\
+    --name=wine\
+    --shm-size=1g\
+    --workdir=/home/wineuser\
+    --env=TZ=Asia/Shanghai\
+    --volume=winehome:/home/wineuser\
+    -p 8083:8083\
+    scottyhardy/docker-wine:latest\
+    wine /workspace/cache/nwjs-sdk-v$nw_version-win-x64/nw.exe
+
+sleep 10
+curl http://localhost:8083
+# curl http://localhost:8083/close
+# docker stop wine
--- test/spec/wcc/module/module.spec.ts
+++ test/spec/wcc/module/module.spec.ts
@@ -4,16 +4,12 @@ import path from 'path';
 import linux from '../../../runner/module-linux'
 import windows from '../../../runner/module-windows'
 import * as fs from 'fs'
-import { execFileSync, execSync } from 'child_process';
+import { execFileSync } from 'child_process';
 
 describe("wcc - module", function () {
     this.beforeAll(() => {
         // TODO: 启动docker wine容器
-        execFileSync(path.resolve(__dirname, './nwjs-download.sh'))
-
-        const rootDir = path.resolve(__dirname, '../../../')
-        const cmd = `docker run -it --volume=${rootDir}:/workspace --env=USE_XVFB=yes --env=XVFB_SERVER=:95 --env=XVFB_SCREEN=0 --env=XVFB_RESOLUTION=320x240x8 --env=DISPLAY=:95 --rm --hostname=DESKTOP-1TV4OAG --name=wine --shm-size=1g --workdir=/home/wineuser --env=TZ=Asia/Shanghai --volume=winehome:/home/wineuser -p 8083:8083 scottyhardy/docker-wine:latest wine /workspace/nwjs-sdk-v0.55.0-win-x64/nw.exe`
-        execSync(cmd)
+        execFileSync(path.resolve(__dirname, '../../../runner/nwjs/wine-prepare.sh'), { stdio: 'inherit' })
     })
     describe("llw: linux output should deep equal with wine", function () {
         // afterEach(function(){
