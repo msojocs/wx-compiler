99b8cc7ffb239196da934103c928c258d4a64b65
msojocs <jiyecafe@gmail.com>
2025-01-22 13:19:07 +0800

    fix: 兼容clang

--- tools/gcc-540.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-wget https://ftp.gnu.org/gnu/gcc/gcc-5.4.0/gcc-5.4.0.tar.gz
-sudo mkdir /usr/local/gcc
-tar -zxf gcc-5.4.0.tar.gz
-cd gcc-5.4.0
-./contrib/download_prerequisites
-./configure --prefix=/usr/local/gcc/gcc-5.4.0 --enable-threads=posix --disable-checking --disable-multilib --enable-languages=c,c++ --disable-libsanitizer
-make
-sudo make install
