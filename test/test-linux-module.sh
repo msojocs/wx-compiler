f31c7dbe8f45cf5f650869cce419d0760b18f098
msojocs <jiyecafe@gmail.com>
2025-02-16 22:43:43 +0800

    fix: wcsc 索引与 一个括号 (#12)
    * fix: wine environment
    
    * fix: wine environment
    
    * fix: environment
    
    * perf: remove recommend
    
    * fix: env
    
    * Update release.yml
    
    * fix: permission of wcsc.exe
    
    * fix: 一处越界问题
    
    * fix: pnpm install
    
    * fix: wine prepare
    
    * fix: wine cfg
    
    * feat: error console
    
    * fix: 兼容clang
    
    * perf: 调整位置
    
    * perf: 带入环境
    
    * fix: version
    
    * perf: log
    
    * fix: version check
    
    * fix: version
    
    * fix: wcsc 索引与 一个括号
    处理一处索引出现负数的问题；
    一处css输出了多余的括号。
    
    * fix: ci

---
+++ test/test-linux-module.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/home/msojocs/github/wx-compiler/cache/nwjs-sdk-v0.55.0-linux-x64/nw /home/msojocs/github/wx-compiler/test/runner/nwjs/compiler.js wcsc /home/msojocs/github/wx-compiler/test/spec/issue/129/data/example.json
